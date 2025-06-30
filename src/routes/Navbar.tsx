b5b36c72af6ad46f41a5603584bb972c5f25b3e1
aminumuhammad000 <aminumuhammad00015@gmail.com>
2025-06-30 17:24:49 +0100

    logo issues fixed

--- src/routes/Navbar.tsx
+++ src/routes/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import style from "./Navbar.module.css";
-import logo from "../assets/Images/logo.png";
+import logo from "../assets/images/logo.jpg";
 import { useState } from "react";
 
 const routes = [
